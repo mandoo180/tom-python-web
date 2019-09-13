0528d4cf4b6b425593b7e1d58844439974507320
Tom <mandoo180@gmail.com>
2019-09-14 03:32:02 +0900

    python flask app

---
+++ data.py
@@ -0,0 +1,32 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'First Article',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam adipisci',
+            'author': 'Faye huhu',
+            'create_date': '2019-09-17'
+        },
+        {
+            'id': 2,
+            'title': 'First Two',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam adipisci um dolor sit amet consectetur adipisicing elit. Quisquam um dolor sit amet consectetur adipisicing elit. Quisquam ',
+            'author': 'Faye huhu',
+            'create_date': '2019-09-07'
+        },
+        {
+            'id': 3,
+            'title': 'First Three',
+            'body': 'Lorem, ipsum dolor sit amet um dolor sit amet consectetur adipisicing elit. Quisquam consectetur adipisicing elit. Quisquam adipisci',
+            'author': 'Faye huhu',
+            'create_date': '2019-09-11'
+        },
+        {
+            'id': 4,
+            'title': 'First Four',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam adipiscum dolor sit amet consectetur adipisicing elit. Quisquam i',
+            'author': 'Faye huhu',
+            'create_date': '2019-09-19'
+        }
+    ]
+    return articles
